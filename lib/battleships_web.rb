d3b77e3c597c4fc1db166b7247b9f837a38a4e33
Alexander Person <alexander@Leonards-MacBook-Pro.local>
2015-09-16 11:31:00 +0100

    Beginning Wednesday, added grid, one cell per row

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,6 +1,15 @@
 require 'sinatra/base'
+require_relative 'water'
+require_relative 'ship'
+require_relative 'player'
+require_relative 'game'
+require_relative 'cell'
+require_relative 'board'
 
 class BattleshipWeb < Sinatra::Base
+  
+  set :views, proc { File.join(root, '..', 'views')}
+
   get '/' do
     erb :index
   end
@@ -11,10 +20,10 @@ class BattleshipWeb < Sinatra::Base
   end
 
   get '/board' do
+    @board = Board.new(Cell)
   	erb :board
   end
 
-  set :views, proc { File.join(root, '..', 'views')}
   # start the server if ruby file executed directly
   run! if app_file == $0
 
