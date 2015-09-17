d551d872fa0a3dd5301237c867ef2e7ec46eb17c
adellanno <anthony@dellanno.co.uk>
2015-09-17 11:49:34 +0100

    initial commit

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -7,7 +7,7 @@ require_relative 'cell'
 require_relative 'board'
 
 class BattleshipWeb < Sinatra::Base
-  
+
   set :views, proc { File.join(root, '..', 'views')}
 
   get '/' do
@@ -21,8 +21,20 @@ class BattleshipWeb < Sinatra::Base
 
   get '/board' do
     @board = Board.new(Cell)
+    @coordinates = params[:coordinates]
+    @ship_type = params[:ship_type]
+    @orientation = params[:orientation]
   	erb :board
   end
+
+
+
+
+  # def place(ship, coord, orientation = :horizontally)
+  #   coords = [coord]
+  #   (ship.size - 1).times{coords << next_coord(coords.last, orientation)}
+  #   put_on_grid_if_possible(coords, ship)
+  # end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
